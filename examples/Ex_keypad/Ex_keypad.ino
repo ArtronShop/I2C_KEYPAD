f3c23c0ccb475336c5fd16835fed509150d2dac2
max30012540 <max30012540@hotmail.com>
2017-12-23 02:12:07 +0700

    Add on() method and support change SDA and SCL pin

--- examples/Ex_keypad/Ex_keypad.ino
+++ examples/Ex_keypad/Ex_keypad.ino
@@ -1,18 +1,31 @@
-#include "i2c_keypad.h"
-I2CKEYPAD keypad;
-void setup() 
-{
+/* Create by IOXhop : www.ioxhop.com */
+
+#include <i2c_keypad.h>
+
+I2CKEYPAD key;
+// I2CKEYPAD key(21, 22); // if you use ESP8266 or ESP32 you can change SDA and SCL pin
+
+void setup() {
   Serial.begin(9600);
-  keypad.begin();
-  keypad.set_event(PRESS); // PRESS, RELEASE, DO
-  keypad.keypadevent = keyevent;
-  
+  Serial.println("Start Keypad");
+    
+  key.begin(0x20, 100); // void begin(char addr = 0x20, long interval = 200) ;
+  key.on(PRESS, [](char key) { // void on(KEYPAD_EVENT event, KeypadEventCallback callback) ;
+    Serial.print("PRESS: ");
+    Serial.println(key);
+  });
+  /*
+  key.on(DO, [](char key) {
+    Serial.print("DO: ");
+    Serial.println(key);
+  });
+  key.on(RELEASE, [](char key) {
+    Serial.print("RELEASE: ");
+    Serial.println(key);
+  });
+  */
 }
-void loop() 
-{
-  keypad.scand(100); //scan time(ms)
-}
-void keyevent(char key)
-{
-  Serial.println(key);
+
+void loop() {
+  key.scand();
 }
--- i2c_keypad.cpp
+++ i2c_keypad.cpp
@@ -1,54 +1,49 @@
 /*
-i2c_keypad by ThaiEasyElec.com 
+Modify by IOXhop : www.ioxhop.com
+
+Original i2c_keypad by ThaiEasyElec.com 
 www.thaieasyelec.com
 https://github.com/ThaiEasyElec/
 support@thaieasyelec.com
 */
 
+#ifndef i2c_keypad_cpp
+#define i2c_keypad_cpp
+
 #include "i2c_keypad.h"
-#include <Arduino.h>
-#include <Wire.h>
 
-unsigned long 	kp_pv = 0; 
-unsigned char last_key=0xFF;
-void key_null(char k){};
+#if defined(ESP32) || defined(ESP8266)
+I2CKEYPAD::I2CKEYPAD(int sda, int scl) : _sda(sda), _scl(scl) { }
+#else
+I2CKEYPAD::I2CKEYPAD() { }
+#endif
 
-I2CKEYPAD::I2CKEYPAD()
-{
-	keypadevent = key_null;
-}
-void I2CKEYPAD::begin(char addr)
-{
+void I2CKEYPAD::begin(char addr, long interval) {
 	addr_ = addr;
-	Wire.begin();   
-	set_event();
+#if defined(ESP32) || defined(ESP8266)
+	Wire.begin(_sda, _scl);
+#else
+	Wire.begin();
+#endif
+	kp_interval = interval;
 }
-void I2CKEYPAD::set_event(unsigned char event)
-{
-	event_ = event;
+
+void I2CKEYPAD::on(KEYPAD_EVENT event, KeypadEventCallback callback) {
+	_callback[event] = callback;
 }
-void I2CKEYPAD::scand(long kp_interval)
-{
-	//kp_interval = interval;
-	unsigned char recv;
-	char row, col = 0;
-	char keypad[4][4] = {{'1','2','3','A'},
-						 {'4','5','6','B'},
-						 {'7','8','9','C'},
-						 {'*','0','#','D'}};
+
+void I2CKEYPAD::scand() {
+	char row = 0, col = 0;
 	
-	unsigned long kp_current = millis();
-	if (kp_current - kp_pv >= kp_interval) 
-	{
+	if (millis() - kp_pv >= kp_interval) {
+		// row scanning
 		Wire.beginTransmission(addr_);   
 		Wire.write(0x0F);               
 		Wire.endTransmission();
 		Wire.requestFrom(addr_, 1);
-		while(Wire.available())
-		{
-			recv = Wire.read();
-			switch(recv)
-			{
+		while(Wire.available()) {
+			char recv = Wire.read();
+			switch(recv) {
 				case 0x0E : row = 1; break;
 				case 0x0D : row = 2; break;
 				case 0x0B : row = 3; break;
@@ -57,16 +52,14 @@ void I2CKEYPAD::scand(long kp_interval)
 			}
 		}
 
-		//column scanning
+		// column scanning
 		Wire.beginTransmission(addr_);
 		Wire.write(0xF0);
 		Wire.endTransmission();
 		Wire.requestFrom(addr_, 1);
-		while(Wire.available())
-		{
-			recv = 0x00FF&Wire.read();
-			switch(recv)
-			{
+		while(Wire.available()) {
+			char recv = 0x00FF&Wire.read();
+			switch(recv) {
 				case 0xE0 : col = 1; break;
 				case 0xD0 : col = 2; break;
 				case 0xB0 : col = 3; break;
@@ -75,50 +68,22 @@ void I2CKEYPAD::scand(long kp_interval)
 			}
 		}
 		kp_pv  =  millis();
-		if(row!=0 && col!=0)
-		{
-			if(event_ ==  PRESS)
-			{
-				if(last_key != keypad[row-1][col-1])
-				(*keypadevent)(keypad[row-1][col-1]);
-				last_key = keypad[row-1][col-1];
+		
+		if(row > 0 && col > 0) {
+			if(last_key == 0xFF) {
+				if (_callback[PRESS]) _callback[PRESS](keypad[row-1][col-1]);
 			}
-			if(event_ ==  RELEASE)
-			{
-				if(last_key != keypad[row-1][col-1])
-				{
-					release_ = keypad[row-1][col-1];
-					last_key = 0xFF;
-				}
-					
+			if (last_key =- keypad[row-1][col-1]) {
+				if (_callback[DO]) _callback[DO](keypad[row-1][col-1]);
 			}
-			if(event_ ==  DO)
-			{
-				(*keypadevent)(keypad[row-1][col-1]);
+			last_key = keypad[row-1][col-1];
+		} else {
+			if(last_key != 0xFF) {
+				if (_callback[RELEASE]) _callback[RELEASE](last_key);
+				last_key = 0xFF;
 			}
-			
 		}
-		else
-		{
-			if(event_ ==  RELEASE)
-			{
-				if(last_key != release_)
-				{
-						(*keypadevent)(release_);
-						release_ = 0xFF;
-						last_key = 0xFF;
-				}
-				
-			}
-			else
-			last_key = 0xFF;
-		}
-			 
-			
 	}
 }
 
-
-
-
-
+#endif
